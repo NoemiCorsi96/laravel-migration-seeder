60adc38f3d9af63012b35bdcd7c11528ca942991
NoemiCorsi96 <noemicorsi96@gmail.com>
2026-02-24 11:58:36 +0100

    Add TrainSeeder using Faker to populate trains table

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,13 +5,14 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker; 
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //prima modalità tramite un array e il foreach 
             $trains = [
@@ -45,6 +46,26 @@ class TrainSeeder extends Seeder
             foreach ($trains as $train) {
                 Train::create($train);
             }
+
+        //seconda modalità tramite il faker
+        for ($i = 0; $i < 20; $i++) {
+
+        Train::create([
+            'company' => $faker->randomElement(['Trenitalia', 'Italo']),
+            'departure_station' => $faker->city(),
+            'arrival_station' => $faker->city(),
+
+            'departure_time' => $faker->dateTimeBetween('now', '+7 days'),
+            'arrival_time' => $faker->dateTimeBetween('+7 days', '+14 days'),
+
+            'train_code' => strtoupper($faker->bothify('??###')),
+
+            'carriages' => $faker->numberBetween(5, 12),
+
+            'on_time' => $faker->boolean(80),
+            'cancelled' => $faker->boolean(10),
+        ]);
+    }
     }
 
 }
